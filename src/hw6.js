5fa7db5caf9289a33d6b271b62e4da4d8247a45d
ShavitAmrami <shavitamrami@gmail.com>
2022-06-19 13:45:38 +0300

    Added a comment in line 9

--- src/hw6.js
+++ src/hw6.js
@@ -6,7 +6,7 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-
+// test
 // helper function for later on
 function degrees_to_radians(degrees)
 {
